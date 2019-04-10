244d933c0817f2594c05a7c1464eab5a986ebd3f
succcc <39544952+succcc@users.noreply.github.com>
2019-04-10 20:11:24 +0900

    file dialog with tkinter freq examine from np.argmax

--- mechanical.py
+++ mechanical.py
@@ -2,53 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from lmfit import Parameters
 from lmfit import Model
+import tkinter as tk
+from tkinter import filedialog
 
-f, _, vrms, _, _, _, _, vrms2, pha = np.loadtxt("data/a2_c100h200_313k_torsional_data.csv", delimiter=',', unpack=True, skiprows=3)  # loading first interval
-targetf= 313*1000
+root = tk.Tk()
+root.withdraw()
 
-def lorentzian(f,a, fr, gamma, c):
-    return a/(gamma**2+(f-fr)**2)+c
+file_path = filedialog.askopenfilename()
 
+f, _, vrms, _, _, _, _, vrms2, pha = np.loadtxt(file_path,dtype='float', delimiter=',' , unpack=True, skiprows=3)  # loading first interval
+
+def lorentzian(f,a, fr, gamma):
+    return a/(gamma**2+(f-fr)**2)
+idMax = np.argmax(vrms2)
+fPeak = f[idMax]
+vrmsPeak = vrms2[idMax]
 mod = Model(lorentzian)
 parrs = Parameters()
-
-
-parrs.add('a', value=100, min=0,
+parrs.add('a', value=1, min=0,
+          max=100)
+parrs.add('fr', value=fPeak, min=fPeak-1*1000,
+          max=fPeak+1*1000)
+parrs.add('gamma', value=5, min=0,
           max=10000)
-parrs.add('fr', value=targetf, min=targetf-10*1000,
-          max=targetf+10*1000)
-parrs.add('gamma', value=500, min=10,
-          max=10000)
-parrs.add('c', value=0, min=-10,
-          max=10)
+
 
 result = mod.fit(vrms2, parrs, f=f) # fitting
 best_a = result.best_values['a']
 best_fr = result.best_values['fr']
 best_gamma = result.best_values['gamma']
-best_c = result.best_values['c']
+
 
 print(result.fit_report())
 
-qfactor = best_fr/2/best_gamma
-
 plt.subplot(2,1,1)
 plt.plot(f/1000, vrms2, "o")
-plt.plot(f/1000, lorentzian(f, best_a, best_fr, best_gamma, best_c), "-r",lw=3, label="Q = {:0.2f}".format(qfactor))
-plt.ylabel(r"Signal (A. U.)", fontsize=18)
+plt.plot(f/1000, lorentzian(f, best_a, best_fr, best_gamma), "-r",lw=3)
+plt.ylabel(r"Signal (A. U.)", fontsize=20)
 plt.yticks([],[],fontsize=18)
-plt.xticks([],[],fontsize=18)
-plt.legend()
+plt.xticks(fontsize=18)
+
 
 plt.subplot(2,1,2)
 plt.plot(f/1000, pha, "o")
 plt.ylabel(r"Phase (A. U.)", fontsize=18)
 plt.xticks(fontsize=18)
-plt.locator_params(nbins=5)
 plt.xlabel(r"$f_m$ (kHz)", fontsize=18)
 plt.yticks([],[],fontsize=18)
-
-plt.tight_layout()
 
 plt.show()
 
